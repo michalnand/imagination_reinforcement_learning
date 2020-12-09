1d05f49c5058a47d77bc53ff71505f2992fc59eb
michalnand <michal.nand@gmail.com>
2020-12-09 21:40:56 +0100

    models imagination

---
+++ experiments/half_cheetah/models/ddpg_imagination/run_4/src/model_forward.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+
+import sys
+sys.path.insert(0, '../../../../..')
+
+import libs_layers
+
+
+class Model(torch.nn.Module):
+    def __init__(self, input_shape, outputs_count, hidden_count = 256):
+        super(Model, self).__init__()
+
+        self.device = "cpu"
+        
+        self.layers = [ 
+            nn.Linear(input_shape[0] + outputs_count, hidden_count),
+            nn.ReLU(),
+            nn.Linear(hidden_count, hidden_count),
+            nn.ReLU(),           
+            nn.Linear(hidden_count, input_shape[0])
+        ]
+
+        torch.nn.init.xavier_uniform_(self.layers[0].weight)
+        torch.nn.init.xavier_uniform_(self.layers[2].weight)
+        torch.nn.init.xavier_uniform_(self.layers[4].weight)
+       
+        self.model = nn.Sequential(*self.layers)
+        self.model.to(self.device)
+
+        print("model_forward")
+        print(self.model)
+        print("\n\n")
+       
+    def forward(self, state, action):
+        x = torch.cat([state, action], dim = 1) 
+        return self.model(x) + state
+
+    def save(self, path):
+        torch.save(self.model.state_dict(), path + "trained/model_forward.pt")
+
+    def load(self, path):       
+        self.model.load_state_dict(torch.load(path + "trained/model_forward.pt", map_location = self.device))
+        self.model.eval()  
+    
